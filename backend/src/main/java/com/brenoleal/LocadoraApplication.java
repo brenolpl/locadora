b276940fa6529d9f7b21e70ab1137d6d419a47ac
Breno Luiz <brenoluizleal@gmail.com>
2022-08-10 13:54:39 -0300

    First Commit

---
+++ backend/src/main/java/com/brenoleal/LocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.brenoleal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraApplication.class, args);
+	}
+
+}
