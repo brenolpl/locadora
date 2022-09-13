e54d0177d8846321f6d1f96d818bcfc140f9cf5c
breno.leal <breno.leal@vixteam.com.br>
2022-09-13 19:16:43 -0300

    Criação de controller genérico e inclusão dos endpoints de classificação e diretor.

--- backend/src/main/java/com/locadoar/backend/controller/BaseController.java
+++ backend/src/main/java/com/locadoar/backend/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.locadoar.backend.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public abstract class BaseController<DOMAIN, REPOSITORY extends JpaRepository<DOMAIN, Integer>> {
     private Class domainClass;
     private REPOSITORY repository;
