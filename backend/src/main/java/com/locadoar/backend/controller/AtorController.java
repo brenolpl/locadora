760426b306a32af4cf1806865e6cdef8e1b74a53
Breno Luiz <brenoluizleal@gmail.com>
2022-09-13 13:34:54 -0300

    Get by id do ator

--- backend/src/main/java/com/locadoar/backend/controller/AtorController.java
+++ backend/src/main/java/com/locadoar/backend/controller/AtorController.java
@@ -5,6 +5,7 @@ import com.locadoar.backend.persistence.repository.IAtorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/ator")
@@ -19,6 +21,11 @@ public class AtorController {
 
     @Autowired
     private IAtorRepository atorRepository;
+
+    @GetMapping("{id}")
+    public Optional<Ator> getById(@PathVariable Integer id) {
+        return atorRepository.findById(id);
+    }
 
     @GetMapping("list")
     public List<Ator> list() {
