88c955764115ad4c0bfaaba866f0532cf32f66de
Mikael Botassi de Oliveira <elkamilive@gmail.com>
2022-12-12 08:29:20 -0300

    filtragem feita

--- frontend/src/views/titulo/tituloView.vue
+++ frontend/src/views/titulo/tituloView.vue
@@ -1,6 +1,26 @@
 <template>
     <main class="d-flex align-items-center justify-content-center shadow">
         <div class="container">
+            <div class="d-flex align-items-center gap-3 mb-5">
+                <div class="form-floating col-sm-6">
+                    <input type="text" class="form-control" id="filtro" @input="changeList()" ref="filtro">
+                    <label for="filtro" class="form-label">Filtro</label>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div>
+                        <input type="radio" class="btn-check" name="options" id="options-nome" autocomplete="off" checked @change="changeList()" >
+                        <label class="btn btn-outline-primary" for="options-nome">Nome</label>
+                    </div>
+                    <div>
+                        <input type="radio" class="btn-check" name="options" id="options-categoria" autocomplete="off" @change="changeList()">
+                        <label class="btn btn-outline-primary" for="options-categoria">Categoria</label>
+                    </div>
+                    <div>
+                        <input type="radio" class="btn-check" name="options" id="options-ator" autocomplete="off" @change="changeList()">
+                        <label class="btn btn-outline-primary" for="options-ator">Ator</label>
+                    </div>
+                </div>
+            </div>
             <header class="d-flex align-items-center justify-content-between mb-5">
                 <h1 class="text-dark">Lista de Títulos</h1>
                 <button class="btn btn-primary fw-bold" @click="isOpen = true">Adicionar</button>
@@ -12,7 +32,7 @@
                     <th class="col-sm-4">Diretor</th>
                 </my-thead>
                 <tbody>
-                    <my-tr v-for="(titulo, i) in entities" :key="i" :id="titulo.id" :table="'classe'" @delete="deleteElement(titulo.id)" @edit="showFormEdit(titulo.id)">
+                    <my-tr v-for="(titulo, i) in filteredList" :key="i" :id="titulo.id" :table="'classe'" @delete="deleteElement(titulo.id)" @edit="showFormEdit(titulo.id)">
                         <td>{{titulo.nome}}</td>
                         <td>{{titulo.ano}}</td>
                         <td>{{titulo.diretor?.nome || ''}}</td>
@@ -25,7 +45,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, watch } from 'vue'
 import TituloForm from './tituloForm.vue';
 import useRequests from '@/composables/requests';
 import Titulo from '@/models/titulo';
@@ -38,6 +58,8 @@ export default defineComponent({
         const { entities, getAll, destroy, resetEntity } = useRequests(Titulo);
         const isOpen = ref(false);
         const editedId = ref(0);
+        const filteredList = ref([]);
+        const filtro = ref(null);
 
         function showFormEdit(id:any){
             editedId.value = id;
@@ -55,6 +77,32 @@ export default defineComponent({
             getAll();
         }
 
+        const changeList = () => {
+            const filterValue = filtro.value?.value;
+            if(filterValue !== ""){
+                if(document.getElementById("options-nome")?.checked){
+                    filteredList.value = entities.value.filter(titulo => titulo.nome.toLowerCase().includes(filterValue.toLowerCase()));
+                }
+                else if(document.getElementById("options-categoria")?.checked){
+                    filteredList.value = entities.value.filter(titulo => titulo.categoria.toLowerCase().includes(filterValue.toLowerCase()));
+                }
+                else if(document.getElementById("options-ator")?.checked){
+                    filteredList.value = entities.value.filter((titulo) => {
+                        const atores = titulo.atores;
+                        const atoresFiltrados = atores.filter((ator) => {
+                            return ator.nome.toLowerCase().includes(filterValue.toLowerCase())
+                        })
+                        return atores.includes(atoresFiltrados[0]);
+                    });
+                }
+            }
+            else filteredList.value = entities.value;
+        }
+
+        watch(() => entities.value, (newVal) => {
+            if(newVal != null) filteredList.value = entities.value;
+        });
+
         const cancelChange = () => {
             isOpen.value = !isOpen.value;
             editedId.value = 0;
@@ -62,7 +110,7 @@ export default defineComponent({
 
         onMounted(getAll);
 
-        return {isOpen, entities, getAll, refreshList, deleteElement, editedId, showFormEdit, cancelChange}
+        return {isOpen, entities, getAll, refreshList, deleteElement, editedId, showFormEdit, cancelChange, filteredList, changeList, filtro}
     },
 })
 </script>
